8c8d450f083d8e6fac34cdb29aa3ee21e7023993
prthomas808 <prthomas808@gmail.com>
2024-07-16 12:13:29 -0400

    TabBars created

---
+++ ShoppingApp/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  ShoppingApp
+//
+//  Created by Pedro Thomas on 7/16/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Home")
+    }
+}
+
+#Preview {
+    HomeView()
+}
---
+++ ShoppingApp/SearchView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchView.swift
+//  ShoppingApp
+//
+//  Created by Pedro Thomas on 7/16/24.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text("Search")
+    }
+}
+
+#Preview {
+    SearchView()
+}
