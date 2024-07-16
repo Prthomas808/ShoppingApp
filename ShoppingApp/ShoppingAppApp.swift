5a039877be3f2e2577d7d577512bedf1bad7c4d2
prthomas808 <prthomas808@gmail.com>
2024-07-16 11:53:35 -0400

    Initial Commit

---
+++ ShoppingApp/ShoppingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingAppApp.swift
+//  ShoppingApp
+//
+//  Created by Pedro Thomas on 7/16/24.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
